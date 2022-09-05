89abea0cfcaa3c868446c06f9bad5b6d305d6f86
David dos Santos Machado <david.desenvolvedor@gmail.com>
2022-09-05 19:26:57 -0300

    ✨ Adicionando rotas

---
+++ app/Http/Controllers/DirectiveController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Directive;
+use Illuminate\Http\Request;
+
+class DirectiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Directive $directive
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Directive $directive)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Directive $directive
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Directive $directive)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Directive $directive
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Directive $directive)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Directive $directive
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Directive $directive)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\{
+    ProjectController,
+    PurposeController,
+    DirectiveController,
+    ObjectiveController,
+    MetricController
+};
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,5 +26,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('projetos', ProjectController::class)->names('projects')->scoped(['project' => 'id']);
+Route::resource('propositos', PurposeController::class)->names('purposes')->scoped(['purpose' => 'id']);
+Route::resource('diretrizes', DirectiveController::class)->names('directives')->scoped(['directive' => 'id']);
+Route::resource('objetivos', ObjectiveController::class)->names('objectives')->scoped(['objective' => 'id']);
+Route::resource('metricas', MetricController::class)->names('metrics')->scoped(['metric' => 'id']);
 
 require __DIR__.'/auth.php';
